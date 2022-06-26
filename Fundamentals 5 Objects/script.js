c59c82a22f7c238dbd6456f9c57e9b156fea7e26
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-26 11:26:07 +0200

    practice object keys

---
+++ Fundamentals 5 Objects/script.js
@@ -0,0 +1,13 @@
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true,
+};
+
+console.log(user.name); //string
+console.log(user.age);  //number
+console.log(user.isAdmin); //boolean
+
+//practice deleting a key
+// delete user.age;
+// console.log(user.age);
