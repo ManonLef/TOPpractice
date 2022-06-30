0c220eff28278a679e1f4b3e1402675fa030ac3f
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-30 23:09:25 +0200

    add key prompt example

--- Fundamentals 5 Objects/script.js
+++ Fundamentals 5 Objects/script.js
@@ -2,11 +2,16 @@ let user = {
     name: "John",
     age: 30,
     isAdmin: true,
+    "likes noobs": true, //check notation for calling upon this key 
 };
 
 console.log(user.name); //string result = John
 console.log(user.age);  //number result = 30
 console.log(user.isAdmin); //boolean result = true
+console.log(user["likes noobs"]); //true
+
+let key = "age"; // This is useful since you can for example let key = prompt ("what would you like to know?", "age");
+console.log(user[key]);
 //practice deleting a key
 // delete user.age;
 // console.log(user.age);
