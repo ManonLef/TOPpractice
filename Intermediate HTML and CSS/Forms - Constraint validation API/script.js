bc6101f3031c822f4a0a3917ad969f5ace85f00e
Manon <manonlefkos@gmail.com>
2022-11-03 22:30:33 +0100

    add new form setup with novalidate attribute

--- Intermediate HTML and CSS/Forms - Constraint validation API/script.js
+++ Intermediate HTML and CSS/Forms - Constraint validation API/script.js
@@ -1,10 +1,10 @@
-const email = document.getElementById("mail");
+// const email = document.getElementById("mail");
 
-email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity ("I am expecting an e-mail address!");
-    email.reportValidity();
-  } else {
-    email.setCustomValidity("");
-  }
-});
+// email.addEventListener("input", (event) => {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity ("I am expecting an e-mail address!");
+//     email.reportValidity();
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
