c9a35be653e7cbeba2b5d17c92a5045231cac759
Manon <manonlefkos@gmail.com>
2023-02-19 23:48:52 +0100

    add constructor

--- JS course/JS Objects and Constr/script.js
+++ JS course/JS Objects and Constr/script.js
@@ -1,9 +1,33 @@
-const myObject = {
-  property: "Value!",
-  otherProperty: 77,
-  "obnoxious property": function() {
-    console.log("not so noob now")
-  }
-};
+// Objects
 
-const variable = "property"
+// const playerOneName = "Tim"
+// const playerTwoName = "Rufus"
+// const playerOneMarker = "X"
+// const playerTwoMarker = "O"
+
+// const playerOne = {
+//   name: "Tim",
+//   marker: "X"
+// };
+
+// const playerTwo = {
+//   name: "jenn",
+//   marker: "O"
+// }
+
+// function printName(player) {
+//   return player.name
+// }
+
+// Object Constructors
+
+function Player(name, marker) {
+  this.name = name
+  this.marker = marker
+  this.sayName = function() {
+    return "hi, my name is " + name
+  }
+}
+
+const playerOne = new Player("Steve", "X")
+const playerTwo = new Player("Manon", "<3")
