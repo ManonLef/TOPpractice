67f4e7952460e44f4a7987400f808676afb1d03f
Manon <manonlefkos@gmail.com>
2023-02-21 12:31:35 +0100

    add prototype inheritance code with explanations

--- JS course/JS Objects and Constr/script.js
+++ JS course/JS Objects and Constr/script.js
@@ -72,4 +72,28 @@ console.log(aBanana.name); // Banana
 // Uses the showNameAndColor method from the Fruit object prototype, which is Plant.prototype. The aBanana object inherits all the properties and methods from both the Plant and Fruit functions.
 console.log(aBanana.showNameAndColor()); // I am a Banana and my color is yellow.
 
-//howNameAndColor method was inherited by the aBanana object even though it was defined all the way up the prototype chain on the Plant.prototype object.
+//showNameAndColor method was inherited by the aBanana object even though it was defined all the way up the prototype chain on the Plant.prototype object.
+
+
+
+// PRACTICE CODE Object.prototype Properties Inherited by all Objects
+
+function People() {
+  this.superstar = "Michael Jackson";
+}
+// Define "athlete" property on the People prototype so that "athlete" is accessible by all objects that use the People () constructor.
+People.prototype.athlete = "Tiger Woods";
+
+var famousPerson = new People();
+famousPerson.superstar = "Steve Jobs";
+
+// The search for superstar will first look for the superstar property on the famousPerson object, and since we defined it there, that is the property that will be used. Because we have overwritten the famousPerson’s superstar property with one directly on the famousPerson object, the search will NOT proceed up the prototype chain.
+console.log(famousPerson.superstar); // Steve Jobs
+
+// Note that in ECMAScript 5 you can set a property to read only, and in that case you cannot overwrite it as we just did.
+
+// This will show the property from the famousPerson prototype (People.prototype), since the athlete property was not defined on the famousPerson object itself.
+console.log(famousPerson.athlete); // Tiger Woods
+
+// In this example, the search proceeds up the prototype chain and find the toString method on Object.prototype, from which the Fruit object inherited—all objects ultimately inherits from Object.prototype as we have noted before.
+console.log(famousPerson.toString()); // [object Object]
