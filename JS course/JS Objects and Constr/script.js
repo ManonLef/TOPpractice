03bf11a4254f2aece16de976f71a96c1b54c13d5
Manon <manonlefkos@gmail.com>
2023-02-21 13:10:52 +0100

    solution 2 added for exercise 2.

--- JS course/JS Objects and Constr/script.js
+++ JS course/JS Objects and Constr/script.js
@@ -151,4 +151,7 @@ let pockets = {
 // 1. Use __proto__ to assign prototypes in a way that any property lookup will follow the path: pockets → bed → table → head. For instance, pockets.pen should be 3 (found in table), and bed.glasses should be 1 (found in head).
 // 2. Answer the question: is it faster to get glasses as pockets.glasses or head.glasses? Benchmark if needed.
 
-// 1. add proto up the chain
+// 1. add proto up the chain and check in browser console.
+// 2. FALSE head.glasses because it doesn't have to travel up the inheritance chain to search for glasses and will find it in its own properties.
+// 2. CORRECT ANSWER  In modern engines, performance-wise, there’s no difference whether we take a property from an object or its prototype. They remember where the property was found and reuse it in the next request.
+// For instance, for pockets.glasses they remember where they found glasses (in head), and next time will search right there. They are also smart enough to update internal caches if something changes, so that optimization is safe.
