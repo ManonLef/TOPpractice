777a5a57f825cbc542afa1e9e29646cf765374e8
Manon <manonlefkos@gmail.com>
2023-03-01 17:24:41 +0100

    add factory function practice

--- JS course/JS Factory Functions/script.js
+++ JS course/JS Factory Functions/script.js
@@ -1,6 +1,8 @@
 // code along with lesson https://www.theodinproject.com/lessons/node-path-javascript-factory-functions-and-the-module-pattern
 
 // factory function
+
+"use strict";
 const personFactory = (name, age) => {
   const sayHello = () => console.log("Hello " + `${name}` + "!");
   return { name, age, sayHello };
@@ -15,4 +17,30 @@ const Person = function (name, age) {
   this.age = age;
 };
 //creating a new person Object using the above Object Constructor
-const jeff = new Person("Jeff", 34);
+// const jeff = new Person("Jeff", 34);
+
+// Private Variables and functions
+const FactoryFunction = (string) => {
+  const capitalizeString = () => string.toUpperCase();
+  const printString = () => console.log(`----${capitalizeString()}----`);
+  return { printString };
+};
+
+const taco = FactoryFunction("taco");
+
+taco.printString();
+
+//another example
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+
+const counter = counterCreator();
+
+counter();
+counter();
+counter();
