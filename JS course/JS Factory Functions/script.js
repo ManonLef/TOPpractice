f3aab0194f27dd9b8da54185b92a1f8dd1d17ea8
Manon <manonlefkos@gmail.com>
2023-03-02 14:38:39 +0100

    add practice code alongs

--- JS course/JS Factory Functions/script.js
+++ JS course/JS Factory Functions/script.js
@@ -44,3 +44,29 @@ const counter = counterCreator();
 counter();
 counter();
 counter();
+
+const o1 = { a: 1 };
+const o2 = { b: 2 };
+const o3 = { c: 3 };
+
+const obj = Object.assign({},o1, o2, o3);
+console.log(obj); // { a: 1, b: 2, c: 3 }
+console.log(o1); // { a: 1, b: 2, c: 3 }, target object itself is changed.
+
+
+// another play along from https://medium.com/javascript-scene/3-different-kinds-of-prototypal-inheritance-es6-edition-32d777fa16c9
+const proto = {
+  hello: function hello() {
+    return `Hello, my name is ${ this.name }`;
+  }
+};
+
+const george = Object.assign({}, proto, {name: 'George'});
+
+const msg = george.hello();
+
+console.log(msg); // Hello, my name is George
+
+// learned about mixins in the above article 
+
+// IIFE's Module patterns
