c1dae7696b002bc7de78897b698b28c74f735592
Manon <manonlefkos@gmail.com>
2023-03-09 23:03:21 +0100

    play with more classes

--- JS course/JS Classes/script.js
+++ JS course/JS Classes/script.js
@@ -1,73 +1,71 @@
-/*
-Me just playing around thinking of a way to organize habits into classes
-- Create a habit which can be anything
-- habits can go into different areas like:
-  - hygiene
-    - brush teeth 2x a day
-    - brush teeth 3x a day
-    - shower each day
-  - food
-    - eat veggies
-    - eat greens
-    - no takeout
-  -health
-    - walk for 30 minutes
-    - workout
-    - meditate
-  - hobbies
-    - draw daily
-
-
-1. Habits:
-  - description get and set
-
-*/
-
 class Habits {
-  description;
-  notes;
-  category = "General";
-  subCategory;
-
-  get description() {
-    return `Description: ${this.description}`;
-  }
-  set description(description) {
-    this.description = description;
-  }
-
-  get category() {
-    return this.category;
-  }
-
-  get subCategory() {
-    return this.subCategory;
+  constructor(nom) {
+    this.nom = nom
   }
 
 }
 
 class Hygiene extends Habits {
-  constructor(hygieneHabit) {
-    super();
-    hygieneHabit;
-    this.goal
-    this.subCategory = "Health";
+
+  category() {
+    console.log(`General > ${this.nom}`)
   }
 
-  get habit() {
-    return `Habit: ${hygieneHabit}`;
-  }
-  set habit(habit) {
-    hygieneHabit = habit
+  constructor(subcat) {
+    this.subcat = subcat
   }
 
-  get subCategory() {
-    return `${subCategory}`
-  }
-
-  get hierarchy() {
-    return this.category + " > " + this.subCategory;
+  category() {
+    
   }
 }
 
-const brushTeeth = new Hygiene("brush teeth");
+const brushTeeth = new Hygiene("Brush Teeth");
+brushTeeth.category();
+// class Habits {
+//   description;
+//   notes;
+//   category = "General";
+//   subCategory;
+
+//   get description() {
+//     return `Description: ${this.description}`;
+//   }
+//   set description(description) {
+//     this.description = description;
+//   }
+
+//   get category() {
+//     return this.category;
+//   }
+
+//   get subCategory() {
+//     return this.subCategory;
+//   }
+// }
+
+// class Hygiene extends Habits {
+//   constructor(hygieneHabit) {
+//     super();
+//     hygieneHabit;
+//     this.goal;
+//     this.subCategory = "Health";
+//   }
+
+//   get habit() {
+//     return `Habit: ${hygieneHabit}`;
+//   }
+//   set habit(habit) {
+//     hygieneHabit = habit;
+//   }
+
+//   get subCategory() {
+//     return `${subCategory}`;
+//   }
+
+//   get hierarchy() {
+//     return this.category + " > " + this.subCategory;
+//   }
+// }
+
+// const brushTeeth = new Hygiene("brush teeth");
