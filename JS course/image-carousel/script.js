4ab080d5218ee38250024f282d0d5896dc00a89c
Manon <manonlefkos@gmail.com>
2023-04-15 23:58:21 +0200

    add image slider next function

--- JS course/image-carousel/script.js
+++ JS course/image-carousel/script.js
@@ -1,13 +1,28 @@
-// Once you have the slider positioned properly, 
-// build functions for “next” and “previous” which will advance to the next or previous slide accordingly. 
+// Once you have the slider positioned properly,
+// build functions for “next” and “previous” which will advance to the next or previous slide accordingly.
 // Make the transitions smooth using simple effects.
 
 // if click on next
 // hide this image
 // display next image
 
-const next = document.querySelector(".right-arrow")
+let currentImage = document.querySelector(".active");
+const next = document.querySelector(".right-arrow");
+
+next.addEventListener("click", displayNext);
 
 function displayNext() {
+  const activeImage = document.querySelector(".active");
+  
+  if (activeImage.nextElementSibling) {
+    const nextImage = activeImage.nextElementSibling;
 
+    nextImage.className = "active";
+    activeImage.className = "";
+
+    activeImage.setAttribute("hidden", true);
+    nextImage.removeAttribute("hidden");
+
+    currentImage = nextImage;
+  }
 }
