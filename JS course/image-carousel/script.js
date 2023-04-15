f0e83fe4bdef53b8c3da6e75e82add6d992d6fa7
Manon <manonlefkos@gmail.com>
2023-04-15 23:25:30 +0200

    add pseudo for next function

---
+++ JS course/image-carousel/script.js
@@ -0,0 +1,13 @@
+// Once you have the slider positioned properly, 
+// build functions for “next” and “previous” which will advance to the next or previous slide accordingly. 
+// Make the transitions smooth using simple effects.
+
+// if click on next
+// hide this image
+// display next image
+
+const next = document.querySelector(".right-arrow")
+
+function displayNext() {
+
+}
