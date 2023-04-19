18a3b10d1ce74294f951c695815278947988c0d6
Manon <manonlefkos@gmail.com>
2023-04-19 22:38:46 +0200

    add mail validation

--- JS course/Constraint-Validation-API/script.js
+++ JS course/Constraint-Validation-API/script.js
@@ -1,9 +1,30 @@
+const form = document.querySelector("form");
 const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
 
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("Please enter a valid email");
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
   } else {
-    email.setCustomValidity("");
+    showError();
   }
 });
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Please enter an email address";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value must be an email address";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `email should be at least ${email.minLength} characters; you entered ${email.value.length}`;
+  }
+  emailError.className = "error active";
+}
