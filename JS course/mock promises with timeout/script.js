2b570ca885630290374bbfec31baa6b651130d36
Manon <manonlefkos@gmail.com>
2023-05-11 11:47:44 +0200

    add ,catch and .finally for Promise

--- JS course/mock promises with timeout/script.js
+++ JS course/mock promises with timeout/script.js
@@ -12,3 +12,32 @@ new Promise(function(resolve, reject) {
 })
 .then(function(e) { console.log('done', e); })
 .catch(function(e) { console.log('catch: ', e); });
+
+// own note: error constructor info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/Error
+
+// finally gets called regardless of success or failure
+(new Promise((resolve, reject) => { reject("Nope"); }))
+    .then(() => { console.log("success") })
+    .catch(() => { console.log("fail") })
+    .finally(res => { console.log("finally") });
+
+// >> fail
+// >> finally
+
+
+var req1 = new Promise(function(resolve, reject) { 
+	// A mock async action using setTimeout
+	setTimeout(function() { resolve('First!'); }, 4000);
+});
+var req2 = new Promise(function(resolve, reject) { 
+	// A mock async action using setTimeout
+	setTimeout(function() { reject('Second!'); }, 3000);
+});
+Promise.all([req1, req2]).then(function(results) {
+	console.log('Then: ', results);
+}).catch(function(err) {
+	console.log('Catch: ', err);
+});
+
+// From the console:
+// Catch: Second!
