bae8990223d45f19a9c0ab953800a717eeefdd01
Manon <manonlefkos@gmail.com>
2023-05-11 09:51:57 +0200

    add error catch for testing

--- JS course/mock promises with timeout/script.js
+++ JS course/mock promises with timeout/script.js
@@ -5,3 +5,10 @@ new Promise(function(resolve, reject) {
 .then(function(num) { console.log('first then: ', num); return num * 2; })
 .then(function(num) { console.log('second then: ', num); return num * 3; })
 .then(function(num) { console.log('last then: ', num);});
+
+new Promise(function(resolve, reject) {
+	// A mock async action using setTimeout
+	setTimeout(function() { reject(Error('Data could not be found')); }, 3000);
+})
+.then(function(e) { console.log('done', e); })
+.catch(function(e) { console.log('catch: ', e); });
