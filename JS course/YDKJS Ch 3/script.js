4bd8f4a03a459a68bf2b733e52b2a81f9619a64e
Manon <manonlefkos@gmail.com>
2023-05-12 22:51:37 +0200

    update example with functions

--- JS course/YDKJS Ch 3/script.js
+++ JS course/YDKJS Ch 3/script.js
@@ -1,23 +1,50 @@
-function add(getX,getY,cb) {
-	var x, y;
-	getX( function(xVal){
-		x = xVal;
-		// both are ready?
-		if (y != undefined) {
-			cb( x + y );	// send along sum
-		}
-	} );
-	getY( function(yVal){
-		y = yVal;
-		// both are ready?
-		if (x != undefined) {
-			cb( x + y );	// send along sum
-		}
-	} );
+function add(getX, getY, cb) {
+  var x, y;
+  //////
+  log(x);
+  log(y);
+  log(getX);
+  log(getY);
+  log(cb);
+  //////
+  getX(function (xVal) {
+		log("getting x")
+		log("xval: " + xVal)
+    x = xVal;
+    // both are ready?
+    if (y != undefined) {
+      cb(x + y); // send along sum
+    }
+  });
+  getY(function (yVal) {
+		log("getting Y")
+		log("yval: " + yVal)
+    y = yVal;
+    // both are ready?
+    if (x != undefined) {
+      cb(x + y); // send along sum
+    }
+  });
 }
 
 // `fetchX()` and `fetchY()` are sync or async
 // functions
-add( fetchX, fetchY, function(sum){
-	console.log( sum ); // that was easy, huh?
-} );
+add(fetchX, fetchY, function (sum) {
+  console.log(sum); // that was easy, huh?
+});
+
+// own additions
+
+function log(msg) {
+  console.log(msg);
+}
+
+function fetchX(cb) {
+	setTimeout(() => {
+		cb(5)
+	}, 500);
+}
+
+function fetchY(cb) {
+  cb(100)
+}
