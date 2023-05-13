d9afa9ddb522b77b5814350ddeea8c819a3a40c8
Manon <manonlefkos@gmail.com>
2023-05-13 22:31:54 +0200

    add my explanation to the code

--- JS course/YDKJS Ch 3/script.js
+++ JS course/YDKJS Ch 3/script.js
@@ -1,12 +1,5 @@
 function add(getX, getY, cb) {
   var x, y;
-  //////
-  // log(x);
-  // log(y);
-  // log(getX);
-  // log(getY);
-  // log(cb);
-  //////
   getX(function (xVal) {
     log("getting x");
     log("xval: " + xVal);
@@ -50,3 +43,26 @@ function fetchY(funcEval) {
 	log(funcEval)
   funcEval(100);
 }
+
+// So it took me a while to wrap my head around this. Especially how the yVal and xVal are passed.
+// Conclusion of the whole code: the first out of getY or getX will always fail since the other is undefined.
+// Then when the other one is ready, both are, and it will pass the sum.
+
+// when the function is invoked, a call back to fetchX and fetchY gets passed, and a function as the 3rd parameter
+// the callback fetchX as parameter will be called getX in the actual add function
+// fetchX is a function that takes "cb" as an argument. 
+// if getX is called in the add(..), it will be getX(fuction(xVal) { x = xVal; and if y != undefined; cb(x+y)})
+// fetchX(cb) is the same as getX(function(xVal)) so function(xVal) will be the parameter cb passed to fetchX
+// fetchX(function(xVal){}) translates to:
+// without the timeout:
+// fetchX(function passed...) 
+// function(5)
+// back to the getX function passed:
+// function(5) {
+//  x = 5
+//  if ( y != undefined ) {
+//     cb( 5 + y )
+//    // cb being the passed function that will console.log(sum)
+// } 
+// }
+// }
