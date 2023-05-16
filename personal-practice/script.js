dec29d195101de8831fad47f4290c7005bef5423
Manon <manonlefkos@gmail.com>
2023-05-16 14:35:45 +0200

    add array map example

--- personal-practice/script.js
+++ personal-practice/script.js
@@ -1,14 +1,21 @@
-const test = document.querySelector(".test")
+const test = document.querySelector(".test");
 
-const arr = ["blu", "blu", "blu", "white", "blu", "red", "yellow"]
+const arr = ["blu", "blu", "blu", "white", "blu", "red", "yellow"];
 
 // filter
-const arrFilter = arr.filter(word => word.length > 3)
+const testFilter = document.createElement("div");
+test.appendChild(testFilter);
 
-const testFilter = document.createElement("div")
-test.appendChild(testFilter)
+const arrFilter = arr.filter((word) => word.length > 3);
+const arrFilterTwo = arr.filter((word) => word.length <= 3);
+testFilter.textContent =
+  "filter: " + arrFilter + " > other filter: " + arrFilterTwo;
 
-testFilter.textContent = " filter: " + arrFilter 
+//map
+const testMap = document.createElement("div");
+test.appendChild(testMap);
 
-
-
+const arrMap = arr.map((word) =>
+  word === "blu" ? (word = "white") : (word = word)
+);
+testMap.textContent = "map: " + arrMap;
