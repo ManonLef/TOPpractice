21fe3bac7cebf462b2157f3b55b295d79110ed38
Manon <manonlefkos@gmail.com>
2023-05-16 22:38:49 +0200

    add delay-Promise utility code

--- JS course/YDKJS Ch 3/script.js
+++ JS course/YDKJS Ch 3/script.js
@@ -109,17 +109,39 @@
 // 	console.log( v );	// 42
 // } );
 
-  // chained
-  var p = Promise.resolve( 21 );
+// // chained
+// var p = Promise.resolve( 21 );
 
-  p
-  .then( function(v){
-    console.log( v );	// 21
-  
-    // fulfill the chained promise with value `42`
-    return v * 2;
-  } )
-  // here's the chained promise
-  .then( function(v){
-    console.log( v );	// 42
-  } );
+// p
+// .then( function(v){
+//   console.log( v );	// 21
+
+//   // fulfill the chained promise with value `42`
+//   return v * 2;
+// } )
+// // here's the chained promise
+// .then( function(v){
+//   console.log( v );	// 42
+// } );
+
+function delay(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, time);
+  });
+}
+
+delay(100) // step 1
+  .then(function STEP2() {
+    console.log("step 2 (after 100ms)");
+    return delay(200);
+  })
+  .then(function STEP3() {
+    console.log("step 3 (after another 200ms)");
+  })
+  .then(function STEP4() {
+    console.log("step 4 (next Job)");
+    return delay(50);
+  })
+  .then(function STEP5() {
+    console.log("step 5 (after another 50ms)");
+  });
