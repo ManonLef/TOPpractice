9c0d7b9e2a164d22e180f579634f1cccde80c792
Manon <manonlefkos@gmail.com>
2023-05-17 11:44:21 +0200

    add Promise.all example

--- JS course/YDKJS Ch 3/script.js
+++ JS course/YDKJS Ch 3/script.js
@@ -124,24 +124,44 @@
 //   console.log( v );	// 42
 // } );
 
-function delay(time) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(resolve, time);
-  });
-}
+// function delay(time) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(resolve, time);
+//   });
+// }
 
-delay(100) // step 1
-  .then(function STEP2() {
-    console.log("step 2 (after 100ms)");
-    return delay(200);
-  })
-  .then(function STEP3() {
-    console.log("step 3 (after another 200ms)");
-  })
-  .then(function STEP4() {
-    console.log("step 4 (next Job)");
-    return delay(50);
-  })
-  .then(function STEP5() {
-    console.log("step 5 (after another 50ms)");
-  });
+// delay(100) // step 1
+//   .then(function STEP2() {
+//     console.log("step 2 (after 100ms)");
+//     return delay(200);
+//   })
+//   .then(function STEP3() {
+//     console.log("step 3 (after another 200ms)");
+//   })
+//   .then(function STEP4() {
+//     console.log("step 4 (next Job)");
+//     return delay(50);
+//   })
+//   .then(function STEP5() {
+//     console.log("step 5 (after another 50ms)");
+//   });
+
+// Promise.all([..])
+
+var p1 = request( "http://some.url.1/" );
+var p2 = request( "http://some.url.2/" );
+
+Promise.all( [p1,p2] )
+.then( function(msgs){
+	// both `p1` and `p2` fulfill and pass in
+	// their messages here
+	return request(
+		"http://some.url.3/?v=" + msgs.join(",")
+	);
+} )
+.then( function(msg){
+	console.log( msg );
+} );
+
+// with Promise.all it will be only be fulfilled if and when all its constituent promises are fulfilled. If any one of those promises is rejected
+// the main Promise.all will be rejected
