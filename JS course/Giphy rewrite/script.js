fff1c46628be97cdec595de7c3cd156016eb7571
Manon <manonlefkos@gmail.com>
2023-05-21 22:47:07 +0200

    rewrite previous code to async and await

--- JS course/Giphy rewrite/script.js
+++ JS course/Giphy rewrite/script.js
@@ -1,9 +1,16 @@
 const img = document.querySelector('img');
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cats', {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      img.src = response.data.images.original.url;
-    });
-    
+  // fetch('https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cats', {mode: 'cors'})
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(response) {
+  //     img.src = response.data.images.original.url;
+  //   });
+
+async function getCats() {
+  const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cats', {mode: 'cors'})
+  const cats = await response.json();
+  img.src = cats.data.images.original.url
+}
+
+getCats()
