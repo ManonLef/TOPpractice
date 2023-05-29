9857fb7a05541517048c062cda1e1c70b07fa80a
Manon <manonlefkos@gmail.com>
2023-05-29 22:27:23 +0200

    add teacher solution and notes

--- JS course/CS/Recursion and Linked Lists/collatz.js
+++ JS course/CS/Recursion and Linked Lists/collatz.js
@@ -1,4 +1,4 @@
-// restrictions:
+// restrictions for the collatz CS50 problem linked in the readme:
 
 // if n is 1, stop
 // Otherwise, if n is even, repeat this process on n/2
@@ -8,15 +8,33 @@
 
 function collatz(n, steps = 0) {
   // base case
-  if (n === 1) {
-    return steps
-  } else if (n%2 === 0) {
-    return collatz(n/2, ++steps)
-  } else if (n%2 !== 0) {
-    return collatz(3*n+1, ++steps)
-  };
-};
+  if (n === 1) return steps;
+  // even
+  else if (n % 2 === 0) return collatz(n / 2, ++steps);
+  // odd
+  else if (n % 2 !== 0) return collatz(3 * n + 1, ++steps);
+}
 
-console.log(collatz(50))
+console.log(collatz(27));
 
 // note on my solution: I started with ++steps before the conditionals at first, overlooking the fact that for n===1, there's no more step needed.
+// reformatted to omit the {} since they're not needed on a single return
+
+// Their solution:
+
+function collatzCS(n) {
+  // base
+  if (n == 1) return 0;
+  // even
+  if (n % 2 === 0) return 1 + collatz(n / 2);
+  // odd
+  else return 1 + collatz(3 * n + 1);
+}
+
+
+console.log(collatzCS(27))
+
+// note on their solution: an extra variable for steps is not needed. 
+// they'll just keep returning one for every step until it adds up to the steps including all the recursive calls made. 
+
+// Final note: TODO: Take some time to write out the steps 
