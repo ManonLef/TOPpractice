bfb0a4dad5e1e3016d75de084ba6eea0c2e8b8a2
Manon <manonlefkos@gmail.com>
2023-05-29 21:58:51 +0200

    add base collatz recursion exercise files

---
+++ JS course/CS/Recursion and Linked Lists/collatz.js
@@ -0,0 +1,8 @@
+// restrictions:
+
+// if n is 1, stop
+// Otherwise, if n is even, repeat this process on n/2
+// Otherwise, if n is odd, repeat this process on 3n+1
+
+// Write a recursive function collatz(n) that calculates how many steps it takes to get to 1 if you start from n and recurse as indicated above
+
