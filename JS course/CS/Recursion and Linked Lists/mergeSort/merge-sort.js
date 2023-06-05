c95878c61f842e1aa7f48d699843ba8d37ed5c79
Manon <manonlefkos@gmail.com>
2023-06-05 16:19:57 +0200

    merge-sort functional for some but not all arrays

--- JS course/CS/Recursion and Linked Lists/mergeSort/merge-sort.js
+++ JS course/CS/Recursion and Linked Lists/mergeSort/merge-sort.js
@@ -8,47 +8,76 @@ console.log("ohai merge-sort");
 //  sort right half of elements
 //  merge sorted halves
 
-// so up to the sorting this function makes sense. 
+// so up to the sorting this function makes sense.
 // when we start returning 1 number and merging is where it gets tricky
 // since I think I want to avoid it continuously sorting the merged array.
 // So once the initial array is sorted, I need to keep merging all the results without
 // it going back to sorting
 
-function mergeSort(array) {
-  let length = array.length
-  // step 1: if n < 2 return 
-  if (length < 2) {
-    console.log("returning", array)
+// I got it mostly working with conditionals so will check array prototype methods to see if I can make
+// it failsafe for every order
+
+function mergeSortA(array) {
+  // step 1: if n < 2 return
+  if (array.length < 2) {
+    console.log("returning", array);
     return array;
   }
-  console.log("next", array)
+  console.log("next", array);
   // divide array in half to sort
-  let firstHalf = array.slice(0, length/2)
-  let secondHalf = array.slice(length/2)
-  console.log("first: ", firstHalf)
-  console.log("second: ", secondHalf)
+  let firstHalf = array.slice(0, array.length / 2);
+  let secondHalf = array.slice(array.length / 2);
+  console.log("first: ", firstHalf);
+  console.log("second: ", secondHalf);
   // 2. sort left half
-  let a = mergeSort(firstHalf)
+  let a = mergeSort(firstHalf);
   // 3. sort right half
-  let b = mergeSort(secondHalf)
+  let b = mergeSort(secondHalf);
   // merge sorted halves
-  console.log("a ", a)
-  console.log("b ", b)
-  
-  let indexA = 0
-  let indexB = 0
-  let merged = []
-  if (a[indexA] < b[indexB]) {
-    merged.push(a[indexA], b[indexB])
-    indexA++
+  console.log("a ", a);
+  console.log("b ", b);
+
+  let indexA = 0;
+  let indexB = 0;
+  let merged = [];
+  if (a.length === 1) {
+    if (a[indexA] < b[indexB]) {
+      merged.push(a[0]);
+      merged.push(b[0]);
+    } else {
+      merged.push(b[0]);
+      merged.push(a[0]);
+    }
   } else {
-    merged.push(b[indexB], a[indexA])
-    indexB++
+    while (merged.length < a.length + b.length) {
+      console.log(
+        "*",
+        merged.length < a.length + b.length,
+        "indexA ",
+        indexA,
+        "indexB ",
+        indexB,
+        "a ",
+        a,
+        "b ",
+        b,
+        "merged ",
+        merged
+      );
+
+      if (a[indexA] <= b[indexB]) {
+        merged.push(a[indexA]);
+        indexA++;
+      } else {
+        merged.push(b[indexB]);
+        indexB++;
+      }
+    }
   }
 
-  console.log("returning merged: ", merged)
-  return merged
+  console.log("returning merged: ", merged);
+  return merged;
 }
 
-let unsortedArray = [1, 4, 5, 2, 7, 3, 9, 6];
+let unsortedArray = [3, 4, 6, 5, 7, 2, 1, 8];
 mergeSort(unsortedArray);
