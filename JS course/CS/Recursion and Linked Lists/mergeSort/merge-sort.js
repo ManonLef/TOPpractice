cbe78c3afd6aaf11e69c6c882ba6ca8e63f62187
Manon <manonlefkos@gmail.com>
2023-06-05 12:07:37 +0200

    function update to merge the initial int returns

--- JS course/CS/Recursion and Linked Lists/mergeSort/merge-sort.js
+++ JS course/CS/Recursion and Linked Lists/mergeSort/merge-sort.js
@@ -8,11 +8,16 @@ console.log("ohai merge-sort");
 //  sort right half of elements
 //  merge sorted halves
 
+// so up to the sorting this function makes sense. 
+// when we start returning 1 number and merging is where it gets tricky
+// since I think I want to avoid it continuously sorting the merged array.
+
 function mergeSort(array) {
   let length = array.length
+  // step 1: if n < 2 return 
   if (length < 2) {
     console.log("returning", array)
-    return;
+    return array;
   }
   console.log("next", array)
   // divide array in half to sort
@@ -20,8 +25,27 @@ function mergeSort(array) {
   let secondHalf = array.slice(length/2)
   console.log("first: ", firstHalf)
   console.log("second: ", secondHalf)
-  mergeSort(firstHalf)
-  mergeSort(secondHalf)
+  // 2. sort left half
+  let a = mergeSort(firstHalf)
+  // 3. sort right half
+  let b = mergeSort(secondHalf)
+  // merge sorted halves
+  console.log("a ", a)
+  console.log("b ", b)
+  
+  let indexA = 0
+  let indexB = 0
+  let merged = []
+  if (a[indexA] < b[indexB]) {
+    merged.push(a[indexA], b[indexB])
+    indexA++
+  } else {
+    merged.push(b[indexB], a[indexA])
+    indexB++
+  }
+
+  console.log("merged: ", merged)
+  return merged
 }
 
 let unsortedArray = [1, 4, 5, 2, 7, 3, 9, 6];
